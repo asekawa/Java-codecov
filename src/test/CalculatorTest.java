711c386f417a14b93cd5df92b49dfc9afaa6a6ec
asekawa <asekawar@gmail.com>
2024-04-04 09:34:48 +0530

    Add files

---
+++ src/test/CalculatorTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1, calculator.subtract(3, 2));
+    }
+}
