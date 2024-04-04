a1bc5b055660cefdda4ba608ea60b4595cbd4a77
asekawa <asekawar@gmail.com>
2024-04-04 11:06:23 +0530

    Add files

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -1,20 +1,21 @@
 package com.example;
 
-import static org.junit.Assert.assertEquals;
-
+import junit.framework.TestCase;
 import org.junit.Test;
 
-public class CalculatorTest {
+public class CalculatorTest extends TestCase {
 
     private final Calculator calculator = new Calculator();
 
     @Test
     public void testAdd() {
         assertEquals(5, calculator.add(2, 3));
+        System.out.println("Running testAdd");
     }
 
     @Test
     public void testSubtract() {
+        System.out.println("Running testSubtract");
         assertEquals(1, calculator.subtract(3, 2));
     }
 }
