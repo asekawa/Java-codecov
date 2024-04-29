57f21d53bc444f8bce7d1f9ec283b410ba10f726
Sahan Dilshan <32576163+sahandilshan@users.noreply.github.com>
2024-04-30 01:03:44 +0530

    Update

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -19,14 +19,6 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }  
-    
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-
-    public int divide(int a, int b) {
-        return a / b;
-    }
 
     // Test.
 }
